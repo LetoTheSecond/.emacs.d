d555b8886bfd94bfe4cc552c64d943eb72548e1d
Leto <letothesecond@8chan.co>
2018-09-07 21:06:19 -0500

    Initial Commit

---
+++ init.el
@@ -0,0 +1,51 @@
+;;;;;;;;;; This fixed garbage collection, makes emacs start up faster ;;;;;;;;;;
+
+(setq gc-cons-threshold 402653184
+	gc-cons-percentage 0.6)
+(defvar startup/file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
+
+(defun startup/revert-file-name-handler-alist ()
+	(setq file-name-handler-alist startup/file-name-handler-alist))
+
+(defun startup/reset-gc ()
+	(setq gc-cons-threshold 16777216
+		gc-cons-percentage 0.1))
+
+(add-hook 'emacs-startup-hook 'startup/revert-file-name-handler-alist)
+(add-hook 'emacs-startup-hook 'startup/reset-gc)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(setq package-archives'(
+				("ELPA"  . "http://tromey.com/elpa/")
+				("gnu"   . "http://elpa.gnu.org/packages/")
+				("melpa" . "https://melpa.org/packages/")
+				("org"   . "https://orgmode.org/elpa/")))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+	(package-refresh-contents)
+	(package-install 'use-package))
+
+(when (file-readable-p "~/.emacs.d/config.org")
+	(org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "outline" :family "Consolas")))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+	(popup-kill-ring switch-window async avy ido-describe-bindings smex evil moe-theme powerline which-key use-package))))
