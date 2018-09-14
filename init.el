1f8133195c713623e2e73b9a612906a1c8f0f525
Leto <letothesecond@8chan.co>
2018-09-13 20:14:03 -0500

    Messing around with setting fonts

--- init.el
+++ init.el
@@ -34,7 +34,7 @@
 
 (when (file-readable-p "~/.emacs.d/config.org")
 	(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
-)
+	)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -46,43 +46,49 @@
  '(ansi-color-faces-vector
    [default bold shadow italic underline bold bold-italic bold])
  '(ansi-color-names-vector
-   ["#000000" "#8b0000" "#00ff00" "#ffa500" "#7b68ee" "#dc8cc3" "#93e0e3" "#dcdccc"])
- '(beacon-color "#f2777a")
+   (vector "#373b41" "#cc6666" "#b5bd68" "#f0c674" "#81a2be" "#b294bb" "#8abeb7" "#c5c8c6"))
+ '(beacon-color "#cc6666")
  '(custom-enabled-themes (quote (hemisu-dark)))
  '(custom-safe-themes
    (quote
-	("7feeed063855b06836e0262f77f5c6d3f415159a98a9676d549bfeb6c49637c4" "d1cc05d755d5a21a31bced25bed40f85d8677e69c73ca365628ce8024827c9e3" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
+	("7feeed063855b06836e0262f77f5c6d3f415159a98a9676d549bfeb6c49637c4" "c1fb68aa00235766461c7e31ecfc759aa2dd905899ae6d95097061faeb72f9ee" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "d1cc05d755d5a21a31bced25bed40f85d8677e69c73ca365628ce8024827c9e3" default)))
+ '(fci-rule-color "#BBBBBB")
  '(flycheck-color-mode-line-face-to-color (quote mode-line-buffer-id))
  '(frame-background-mode (quote dark))
  '(package-selected-packages
    (quote
-	(color-theme-sanityinc-tomorrow hemisu-theme cyberpunk-theme which-key use-package switch-window spacemacs-theme smex projectile powerline popup-kill-ring moe-theme ido-grid-mode ido-describe-bindings evil-org dashboard beacon avy async)))
+	(linum-relative key-chord magit color-theme-sanityinc-tomorrow hemisu-theme cyberpunk-theme spacemacs-theme moe-theme powerline popup-kill-ring switch-window async fill-column-indicator avy beacon dashboard page-break-lines projectile org-bullets evil-org evil ido-grid-mode ido-describe-bindings smex which-key use-package)))
  '(vc-annotate-background nil)
  '(vc-annotate-color-map
    (quote
-	((20 . "#f2777a")
-	 (40 . "#f99157")
-	 (60 . "#ffcc66")
-	 (80 . "#99cc99")
-	 (100 . "#66cccc")
-	 (120 . "#6699cc")
-	 (140 . "#cc99cc")
-	 (160 . "#f2777a")
-	 (180 . "#f99157")
-	 (200 . "#ffcc66")
-	 (220 . "#99cc99")
-	 (240 . "#66cccc")
-	 (260 . "#6699cc")
-	 (280 . "#cc99cc")
-	 (300 . "#f2777a")
-	 (320 . "#f99157")
-	 (340 . "#ffcc66")
-	 (360 . "#99cc99"))))
+	((20 . "#cc6666")
+	 (40 . "#de935f")
+	 (60 . "#f0c674")
+	 (80 . "#b5bd68")
+	 (100 . "#8abeb7")
+	 (120 . "#81a2be")
+	 (140 . "#b294bb")
+	 (160 . "#cc6666")
+	 (180 . "#de935f")
+	 (200 . "#f0c674")
+	 (220 . "#b5bd68")
+	 (240 . "#8abeb7")
+	 (260 . "#81a2be")
+	 (280 . "#b294bb")
+	 (300 . "#cc6666")
+	 (320 . "#de935f")
+	 (340 . "#f0c674")
+	 (360 . "#b5bd68"))))
  '(vc-annotate-very-old-color nil))
-
+;(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ ;'(default ((t (:foundry "outline" :family "Courier New")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 1 :width normal :foundry "default" :family "default")))))
+ )
